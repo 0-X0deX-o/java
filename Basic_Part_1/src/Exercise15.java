b0e43b725e1905d29a7994834af6502a5fa75d32
David Liddle <dpliddle@liberty.edu>
2023-01-12 17:17:01 -0600

    Exercise 17

---
+++ Basic_Part_1/src/Exercise15.java
@@ -0,0 +1,16 @@
+class Exercise15
+{
+   public static void main(String[] args)
+    {
+        int a, b, temp;
+        a = 15;
+        b = 27;
+        System.out.println("Before swapping : a, b = " + a + ", "+ b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Before swapping : a, b = " + a + ", "+ b);
+    }
+}
+
+// learn how to assign parameters to a method in java
---
+++ Basic_Part_1/src/Exercise16.java
@@ -0,0 +1,6 @@
+public class Exercise16 {
+    public static void main(String[] args)
+    {
+        System.out.println(" +\"\"\"\"\"+\n[| o o |]\n |  ^  |\n | '-' |\n +-----+\n");
+    }
+}
